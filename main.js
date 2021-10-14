f3996a29db7fb104a2f0c2200c06d3eb5b608f1c
salrey <tekdehn@gmail.com>
2021-10-14 15:12:41 -0400

    Views and changes information upon option selection

--- main.js
+++ main.js
@@ -1,43 +1,92 @@
+const BASE_URL = "https://ghibliapi.herokuapp.com/films/" 
 const movieSelection = document.querySelector("#movie-selection")
 
-fetch("https://ghibliapi.herokuapp.com/films/")
+fetch(BASE_URL)
     .then((response) => response.json())
-    .then((data) => {
+    .then((movies) => {
         //check data
         // console.log(data[0])
-        data.forEach((movie) => {
+        movies.forEach((movie) => {
             const option = document.createElement("option");
             option.value = movie.title.split(" ").join("-").toLowerCase().replace("'", "") 
             //check option.value is formatted correctly
             // console.log(option.value) 
             option.textContent = movie.title
             movieSelection.append(option)
+
+        })
+        //Add event when an option is selected 
+        const options = document.querySelectorAll("option")
+        options.forEach((option) => {
+            option.addEventListener("click", (event) => {
+                
+                //movie found
+                const found = movies.find((movie) => movie.title === option.textContent)
+                
+                if (movieSelection.value !== "default") {
+                    //remove/reset 
+                    document.querySelector("#display-info").classList.add("hidden")
+
+                    document.querySelector("form p").classList.add("error")
+                    document.querySelector("#default-info").classList.remove("hidden")
+                } else {
+
+                    //remove/reset
+                    document.querySelector("#display-info").classList.remove("hidden")
+                    document.querySelector("form p").classList.remove("error")
+                    document.querySelector("#default-info").classList.add("hidden")
+
+    
+                    if (!document.querySelector("#display-info h3")) {
+                        let movieHeader = document.createElement("h3")
+                        movieHeader.textContent = found.title
+                        let movieYear = document.createElement("div")
+                        movieYear.textContent = found.release_date
+                        let movieDescription = document.createElement("p")
+                        movieDescription.textContent = found.description
+                        //Manipulate DOM
+                        document.querySelector("#display-info").append(movieHeader, movieYear, movieDescription)
+                    } else {
+                        document.querySelector("#display-info h3").textContent = found.title 
+                        document.querySelector("#display-info div").textContent = found.release_date 
+                        document.querySelector("#display-info p").textContent = found.description 
+                    }
+                }
+
+            })
         })
     })
 
-document.querySelector("form").addEventListener("submit", (event) => {
-    event.preventDefault();
+// document.querySelector("form").addEventListener("submit", (event) => {
+//     event.preventDefault();
     
-    const input = event.target["movie-review"].value
+//     const input = event.target["movie-review"].value
 
-    //remove/reset old things 
-    document.querySelector("form p").classList.remove("error")
-    document.querySelector("#default-reviews p").classList.remove("error")
-    // document.querySelector("#display-info article").remove() 
-    document.querySelector("#default-info").classList.remove("hidden")
+//     //remove/reset old things 
+//     document.querySelector("form p").classList.remove("error")
+//     document.querySelector("#default-reviews p").classList.remove("error")
 
-    if (movieSelection.value === "default") {
-        document.querySelector("form p").classList.add("error")
-    } else if (!input) {
-        document.querySelector("#default-reviews p").classList.add("error")
-    } else {
-        //reset/remove old things 
-        document.querySelector("form p").classList.remove("error")
-        document.querySelector("#default-reviews p").classList.remove("error")
-        // fetch()
-    }
+//     if (!document.querySelector("#display-info h3")) {
+//         document.querySelector("#default-info").classList.remove("hidden")
+//     }
+
+//     if (movieSelection.value === "default") {
+//         document.querySelector("form p").classList.add("error")
+//     } else if (!input) {
+//         document.querySelector("#default-reviews p").classList.add("error")
+//     } else {
+//         //reset/remove old things 
+//         document.querySelector("form p").classList.remove("error")
+//         document.querySelector("#default-reviews p").classList.remove("error")
+        
+//         fetch(BASE_URL) 
+//             .then((response) => response.json())
+//             .then((movies) => {
+
+//             })
+//     }
     
-    //reset twice to reset both dropdown and text input 
-    event.target.reset();
-    event.target.reset();
-})
+//     //reset twice to reset both dropdown and text input 
+//     // event.target.reset();
+//     // event.target.reset();
+// })
