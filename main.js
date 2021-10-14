139faf575ae9a8e3399d06371b17e4e707352b8c
salrey <tekdehn@gmail.com>
2021-10-14 18:11:25 -0400

    Update event and reset

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ fetch(BASE_URL)
         // console.log(data[0])
         movies.forEach((movie) => {
             const option = document.createElement("option");
-            option.value = movie.title.split(" ").join("-").toLowerCase().replace("'", "") 
+            option.value = movie.title
             //check option.value is formatted correctly
             // console.log(option.value) 
             option.textContent = movie.title
@@ -16,43 +16,33 @@ fetch(BASE_URL)
 
         })
         //Add event when an option is selected 
-        const options = document.querySelectorAll("option")
-        options.forEach((option) => {
-            option.addEventListener("click", () => {
-                
-                //movie found
-                const found = movies.find((movie) => movie.title === option.textContent)
-                //test will not run properly if checking for default option
-                // if (movieSelection.value === "default") {
-                //     //remove/reset 
-                //     document.querySelector("#display-info").classList.add("hidden")
+        movieSelection.addEventListener("change", (event) => {
 
-                //     document.querySelector("form p").classList.add("error")
-                //     document.querySelector("#default-info").classList.remove("hidden")
-                // } else {}
+            //remove/reset
+            document.querySelector("#display-info").classList.remove("hidden")
+            document.querySelector("#default-info p").classList.remove("error")
+            document.querySelector("#default-info").classList.add("hidden")
 
-                    //remove/reset
-                    document.querySelector("#display-info").classList.remove("hidden")
-                    document.querySelector("#default-info p").classList.remove("error")
-                    document.querySelector("#default-info").classList.add("hidden")
-
-    
+            movies.forEach((movie) => {
+                if (event.target.value === movie.title) {
                     if (!document.querySelector("#display-info h3")) {
                         let movieHeader = document.createElement("h3")
-                        movieHeader.textContent = found.title
+                        movieHeader.textContent = movie.title
                         let movieYear = document.createElement("div")
-                        movieYear.textContent = found.release_date
+                        movieYear.textContent = movie.release_date
                         let movieDescription = document.createElement("p")
-                        movieDescription.textContent = found.description
+                        movieDescription.textContent = movie.description
                         //Manipulate DOM
                         document.querySelector("#display-info").append(movieHeader, movieYear, movieDescription)
                     } else {
-                        document.querySelector("#display-info h3").textContent = found.title 
-                        document.querySelector("#display-info div").textContent = found.release_date 
-                        document.querySelector("#display-info p").textContent = found.description 
+                        document.querySelector("#display-info h3").textContent = movie.title 
+                        document.querySelector("#display-info div").textContent = movie.release_date 
+                        document.querySelector("#display-info p").textContent = movie.description 
                     }
+                }
             })
         })
+        
         //Add event when submitting a review  
         document.querySelector("form").addEventListener("submit", (event) => {
             event.preventDefault();
@@ -62,10 +52,8 @@ fetch(BASE_URL)
             //remove/reset old things 
             document.querySelector("#default-info p").classList.remove("error")
             document.querySelector("#default-reviews p").classList.remove("error")
-        
-            // if (!document.querySelector("#display-info h3")) {
-            //     document.querySelector("#default-info").classList.remove("hidden")
-            // }
+            document.querySelector("#display-info").classList.add("hidden")
+            document.querySelector("#default-info").classList.remove("hidden")
         
             if (!input) {
                 document.querySelector("#default-reviews p").classList.add("error")
